439b3b2ed6e320a690854091c0dd3748b2d69a58
wizzy akor <kluverdev@gmail.com>
2020-07-30 11:59:48 -0400

    how to graphql backend with node js

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { PrismaClient } = require("@prisma/client");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+const { PubSub } = require("graphql-yoga");
+const Subscription = require("./resolvers/Subscription");
+const Vote = require("./resolvers/Vote");
+
+const pubsub = new PubSub();
+const prisma = new PrismaClient();
+
+// 2
+
+// 3
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link,
+  Subscription,
+  Vote,
+};
+// 3
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: (request) => {
+    return {
+      ...request,
+      prisma,
+      pubsub,
+    };
+  },
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
