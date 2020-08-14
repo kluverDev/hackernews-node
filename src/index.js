4fb9c0f31756daf3e4852c00998c801b63dcc103
wizzy akor <kluverdev@gmail.com>
2020-08-14 06:35:47 -0400

    getting started. wrote a typedef and few resolvers

--- src/index.js
+++ src/index.js
@@ -1,38 +1,23 @@
 const { GraphQLServer } = require("graphql-yoga");
-const { PrismaClient } = require("@prisma/client");
-const Query = require("./resolvers/Query");
-const Mutation = require("./resolvers/Mutation");
-const User = require("./resolvers/User");
-const Link = require("./resolvers/Link");
-const { PubSub } = require("graphql-yoga");
-const Subscription = require("./resolvers/Subscription");
-const Vote = require("./resolvers/Vote");
 
-const pubsub = new PubSub();
-const prisma = new PrismaClient();
+// 1
+const typeDefs = `
+type Query {
+  info: String!
+}
+`;
+// the exclamation mark at the end of the type String means what you are returning cannot be null
 
 // 2
-
-// 3
 const resolvers = {
-  Query,
-  Mutation,
-  User,
-  Link,
-  Subscription,
-  Vote,
-};
-// 3
-
-const server = new GraphQLServer({
-  typeDefs: "./src/schema.graphql",
-  resolvers,
-  context: (request) => {
-    return {
-      ...request,
-      prisma,
-      pubsub,
-    };
+  Query: {
+    info: () => null,
   },
+};
+
+// 3
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
